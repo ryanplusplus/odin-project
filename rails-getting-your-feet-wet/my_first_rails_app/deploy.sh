943ccf2cfa03b7c91b490e121fc794b1e9bb7aee
Ryan Hartlage <ryanplusplus@gmail.com>
2019-05-30 20:50:18 -0400

    Prep for deployment

--- rails-getting-your-feet-wet/my_first_rails_app/deploy.sh
+++ rails-getting-your-feet-wet/my_first_rails_app/deploy.sh
@@ -1,4 +1,4 @@
 cd ../.. && \
   git subtree push \
     --prefix rails-getting-your-feet-wet/my_first_rails_app \
-    https://git.heroku.com/ryanplusplus-first-rails-application.git master
+    https://git.heroku.com/ryanplusplus-first-rails-app.git master
