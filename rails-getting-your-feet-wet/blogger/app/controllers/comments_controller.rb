78fc22aa1f0c3858658791b3ee5c8af1ec6c4e9c
Ryan Hartlage <ryanplusplus@gmail.com>
2019-06-02 21:05:32 -0400

    Can add and edit tags

--- rails-getting-your-feet-wet/blogger/app/controllers/comments_controller.rb
+++ rails-getting-your-feet-wet/blogger/app/controllers/comments_controller.rb
@@ -1,12 +1,10 @@
 class CommentsController < ApplicationController
+  include CommentsHelper
+
   def create
     @comment = Comment.new(comment_params)
     @comment.article_id = params[:article_id]
     @comment.save
     redirect_to @comment.article
-  end
-
-  def comment_params
-    params.require(:comment).permit(:author_name, :body)
   end
 end
